d966ecf22a82f8ebd68760dca3c368f3ee9e5317
Umesh Unnikrishnan <umeshanfloyd@gmail.com>
2021-12-16 23:30:22 -0300

    Removed unwanted files and added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav className="navbar navbar-expand-sm bg-light">
+      <div className="container-fluid">
+        <ul className="navbar-nav">
+          <li className="nav-item">Val1</li>
+          <li className="nav-item">Val2</li>
+          <li className="nav-item">Val3</li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
