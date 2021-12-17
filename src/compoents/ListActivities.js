72186c8c942c6fb3cf00fa4c25bdc628b1120b12
Umesh Unnikrishnan <umeshanfloyd@gmail.com>
2021-12-17 16:23:32 -0300

    Added modal for editing the entry

--- src/compoents/ListActivities.js
+++ src/compoents/ListActivities.js
@@ -67,7 +67,7 @@ export default function ListActivities() {
         aria-labelledby="exampleModalLabel"
         aria-hidden="true"
       >
-        <div className="modal-dialog">
+        <div className="modal-dialog modal-dialog-centered">
           <div className="modal-content">
             <div className="modal-header">
               <h5 className="modal-title" id="exampleModalLabel">
