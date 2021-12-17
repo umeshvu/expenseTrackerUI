8d4712d099e8be2c0ca3672139f6538b15955010
Umesh Unnikrishnan <umeshanfloyd@gmail.com>
2021-12-17 00:10:32 -0300

    Added Navbar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./compoents/App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
