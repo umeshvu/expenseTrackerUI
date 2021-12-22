a50d77a443b1ccc8b5b20722ac349e49af38c65c
Umesh Unnikrishnan <umeshanfloyd@gmail.com>
2021-12-22 20:37:56 -0300

    Added redux and axios. Also added types

---
+++ src/action/types.js
@@ -0,0 +1,5 @@
+export const GET_ALL_FNA = "GET_ALL_FNA";
+export const GET_FNA_BY_ID = "GET_FNA_BY_ID";
+export const NEW_FNA = "NEW_FNA";
+export const UPDATE_FNA = "UPDATE_FNA";
+export const DELETE_FNA = "DELETE_FNA";
